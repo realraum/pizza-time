1a3388d82759daad6d4eac9f9c0cedb976ce71e1
Tanja <git@tanja.pw>
2025-04-30 21:29:04 +0200

    Fix the regex101 script

--- scripts/ffs.sh
+++ scripts/ffs.sh
@@ -1,2 +1,6 @@
+# Prevent a crash
+mkdir -p private
+touch private/raw-products.json
+
 # For reasons we don't fully understand, regex101.com exports in an annoying manner:
-< /tmp/deppat.json jq '[.[] | { "id":.[0] | .content, "name":.[1] | .content, "description":.[2] | .content, "price":.[3] | .content, }]' > content.json
+< ./private/raw-products.json jq '[.[] | { "id":.[0] | .content, "name":.[1] | .content, "description":.[2] | .content, "price":.[3] | .content, }]' > private/products.json
