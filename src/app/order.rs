c479137e303a4d6097a6c1bddc207c6ff13b69f1
Tanja <git@tanja.pw>
2025-08-30 02:09:12 +0200

    Add rm the old rm button & listing

--- src/app/order.rs
+++ src/app/order.rs
@@ -119,44 +119,6 @@ pub fn Summary() -> impl IntoView {
                                     "New session"
                                 </button>
 
-                                // TODO replace this div with a (specialized?) `PersonCard` component
-                                <div class="flex flex-col bg-green-50 rounded p-1 dark:bg-gray-700 dark:text-gray-200">
-                                    <h2 class="text-xl">"Your selection"</h2>
-                                    <p class="test-sm">"Your name: "{me.name}</p>
-                                    <table>
-                                        <tbody>
-                                            {
-                                                let order = dedup_and_count(me.order.clone());
-                                                order.into_iter()
-                                                    .map(|(pizza, count)| {
-                                                        view! {
-                                                            <tr>
-                                                                <td>{format!("{}x", count)}</td>
-                                                                <td>{pizza.name}</td>
-                                                                <td>{format!("@ {}€", pizza.price.to_string())}</td>
-                                                                <td>
-                                                                    <button
-                                                                        class="bg-red-500 text-white rounded-md p-1 ml-2"
-                                                                        on:click=move |_| {
-                                                                            let pid = pizza.id.clone();
-                                                                            spawn_local(async move {
-                                                                                rm_pizza(pid).await.unwrap();
-                                                                            });
-                                                                            update_signal.set(!update_signal.get());
-                                                                        }
-                                                                    >
-                                                                        "Remove"
-                                                                    </button>
-                                                                </td>
-                                                            </tr>
-                                                        }
-                                                    })
-                                                    .collect::<Vec<_>>()
-                                            }
-                                        </tbody>
-                                    </table>
-                                </div>
-
                                 <h2 class="text-xl">"Your selection"</h2>
                                 <div class="sm:rounded-xl sm:overflow-clip mt-2 sm:mt-4">
                                     <div class="grid grid-cols-1">
